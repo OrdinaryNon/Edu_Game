a2f2e6ac7104754f9868157309213516d38f3e53
OrdinaryNon <kin.critical@gmail.com>
2025-07-25 08:59:39 +0700

    Mathgameaum

---
+++ Math2/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1753408709,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/picture-sheet0.png",
+		"images/picture2-sheet0.png",
+		"images/tick-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
